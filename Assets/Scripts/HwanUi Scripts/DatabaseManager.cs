869875b0bd5600e4db12b8ba10f5265a6b4ef16d
UnityHanYang <89231433+UnityHanYang@users.noreply.github.com>
2024-07-18 02:03:52 +0900

    edit

---
+++ Assets/Scripts/HwanUi Scripts/DatabaseManager.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private static DatabaseManager instance;
+    private MySqlConnection connection;
+
+    private string serverEndPoint = "unity-mysql-rds.ct8w8icaeo26.ap-northeast-2.rds.amazonaws.com";
+    private string databaseName = "RoyaleDatabase";
+    string port = "3306";
+    private string userName = "root";
+    private string password = "abcdgh3076";
+
+    private string connStr;
+
+    public static DatabaseManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("DatabaseManager").AddComponent<DatabaseManager>();
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        connStr = string.Format("server={0};port={1};uid={2};pwd={3};database={4};", serverEndPoint, port, userName, password, databaseName);
+        DontDestroyOnLoad(gameObject);
+        OpenConnection();
+    }
+
+    void OnDestroy()
+    {
+        CloseConnection();
+    }
+
+    public void OpenConnection()
+    {
+        if (connection == null)
+        {
+            connection = new MySqlConnection(connStr);
+        }
+
+        try
+        {
+            connection.Open();
+            Debug.Log("Database connection opened");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("Failed to open database connection: " + ex.Message);
+        }
+    }
+
+    public void CloseConnection()
+    {
+        if (connection != null)
+        {
+            connection.Close();
+            connection = null;
+            Debug.Log("Database connection closed");
+        }
+    }
+
+    public MySqlCommand CreateCommand(string query)
+    {
+        if (connection != null && connection.State == System.Data.ConnectionState.Open)
+        {
+            return new MySqlCommand(query, connection);
+        }
+        else
+        {
+            Debug.LogError("Connection is not open");
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/HwanUi Scripts/SignUp.cs
@@ -0,0 +1,60 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUp : MonoBehaviour
+{
+    public TextMeshPro nickname;
+    public TextMeshPro password;
+
+    public Button loginBtn;
+
+    private void Start()
+    {
+        loginBtn.interactable = false;
+    }
+
+    void Update()
+    {
+        if (int.Parse(nickname.text) < 4 || int.Parse(password.text) < 4)
+            loginBtn.interactable = false;
+
+
+        if (int.Parse(nickname.text) >= 4 && int.Parse(password.text) >= 4)
+            loginBtn.interactable = true;
+
+    }
+
+    public void LoginClick()
+    {
+        CheckDuplicateName(nickname.text);
+    }
+
+    private bool CheckDuplicateName(string name)
+    {
+        try
+        {
+            string nameSelect = string.Format("SELECT count(*) FROM USER WHERE userName = {0}", name);
+
+            MySqlCommand cmd = DatabaseManager.Instance.CreateCommand(nameSelect);
+
+            if (cmd != null)
+            {
+                object result = cmd.ExecuteScalar();
+                int rowCount = Convert.ToInt32(result);
+
+                return rowCount > 0;
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.LogError("Query execution error: " + ex.Message);
+        }
+        return false;
+    }
+}

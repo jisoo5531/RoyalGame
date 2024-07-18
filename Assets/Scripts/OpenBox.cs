a523e64f301b4e9fc63e04f2f4212804761a5832
mj9373 <mj9373@naver.com>
2024-07-18 21:40:47 +0900

    아아아아

---
+++ Assets/Scripts/OpenBox.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour
+{
+    public GameObject Box_Close;
+    public GameObject Box_Open;
+
+    void Start()
+    {
+        Box_Close = GameObject.Find("Box_Close");
+        Box_Open = GameObject.Find("Box_Open");
+    }
+
+    private void Update()
+    {
+        Chanege();
+    }
+
+    private void Chanege()
+    {
+        if (!Box_Close.activeSelf)
+        {
+            Box_Open.SetActive(true);
+        }
+    }
+
+}
--- Assets/Scripts/ShakeBox.cs
+++ Assets/Scripts/ShakeBox.cs
@@ -6,9 +6,22 @@ using UnityEngine.UI;
 
 public class ShakeBox : MonoBehaviour
 {
+    Vector3 targetPosUP = new Vector3(0, 165, 0);
+    Vector3 targetPosDown = new Vector3(0, 0, 0);
 
     public void Start()
     {
-        transform.DOShakeRotation(3);
+        transform.DOShakeRotation(3).OnComplete(BoxUP);
     }
+
+    public void BoxUP()
+    {
+        transform.DOLocalMove(targetPosUP, 0.5f).OnComplete(BoxDown);
+    }
+
+    public void BoxDown()
+    {
+        transform.DOLocalMove(targetPosDown, 0.5f).OnComplete(() => gameObject.SetActive(false));
+    }
+
 }

a9a48ac10646cfcc1eaaddb1a0d4b4be6802e275
UnityHanYang <UnityHanYang@gmail.com>
2024-07-19 19:45:44 +0900

    edit

--- Assets/Scripts/HwanUi Scripts/PasswordFind.cs
+++ Assets/Scripts/HwanUi Scripts/PasswordFind.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -19,5 +21,29 @@ public class PasswordFind : MonoBehaviour
     private bool CheckTextLength(int nicknameLength, int passwordLength)
     {
         return (nicknameLength >= 4 && passwordLength >= 4);
+    }
+
+    private bool CheckEqualsUserName(string nickname)
+    {
+
+        try
+        {
+            string selectName = string.Format("SELECT count(*) FROM USER WHERE userName = '{0}'", nickname);
+
+            MySqlCommand cmd = DatabaseManager.Instance.CreateCommand(selectName);
+
+            if (cmd != null)
+            {
+                object result = cmd.ExecuteScalar();
+                int rowCount = Convert.ToInt32(result);
+
+                return rowCount > 0;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning("Select Query execution error: " + ex.Message);
+        }
+        return false;
     }
 }

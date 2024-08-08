68ae75e663d4df20ecd80eb1812a63d0ecfc8cc9
UnityHanYang <UnityHanYang@gmail.com>
2024-08-08 21:42:47 +0900

    edit

--- Assets/Scripts/HwanUi Scripts/UnitCardData.cs
+++ Assets/Scripts/HwanUi Scripts/UnitCardData.cs
@@ -47,4 +47,4 @@ public enum Range
 {
     Melee,
     Ranged
-}
+}   
--- Assets/Scripts/JIsoo Scripts/Scripts/FSM/UnitAttack.cs
+++ Assets/Scripts/JIsoo Scripts/Scripts/FSM/UnitAttack.cs
@@ -8,7 +8,7 @@ public class UnitAttack : IState<Unit>
     private bool isAttack = false;
     private bool isCoolTime = false;
     private float attackSpeed = 0f;
-    private float attackDelay = 0.27f;
+    private float attackDelay = 0.2f;
 
     public void OperateEnter(Unit sender, float speed, bool isPrince)
     {

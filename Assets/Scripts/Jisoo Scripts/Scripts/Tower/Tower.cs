8c98fc6f3867cc9d265ba4dd757f96edaa1efd45
UnityHanYang <89231433+UnityHanYang@users.noreply.github.com>
2024-08-13 14:39:03 +0900

    edit

--- Assets/Scripts/Jisoo Scripts/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Jisoo Scripts/Scripts/Tower/Tower.cs
@@ -1,6 +1,8 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Tower : MonoBehaviourPunCallbacks
@@ -14,10 +16,42 @@ public class Tower : MonoBehaviourPunCallbacks
 
     public bool isNotOnCannon { get; set; }
 
+    public TargetFollowUnit targetFollowUnit;
+    public UnitCanvasInfo canvasInfo;
+    [HideInInspector] public Animator anim;
+
+
+    protected enum TowerState
+    {
+        Idle,
+        Attack
+    }
+
+
+    protected Dictionary<TowerState, IState<Tower>> dicState = new Dictionary<TowerState, IState<Tower>>();
+    protected StateMachine<Tower> stateMachine;
+
     public GameObject onTopUnit;
 
-    protected virtual void InitData()
-    {
+    protected virtual void InitData() {}
 
-    }
+    //protected virtual void InitStateMachine()
+    //{
+    //    IState<TowerState> idle = new UnitIdle();
+    //    IState<TowerState> attack = new UnitAttack();
+
+    //    dicState.Add(TowerState.Idle, idle);
+    //    dicState.Add(TowerState.Attack, attack);
+
+    //    stateMachine = new StateMachine<TowerState>(this, dicState[TowerState.Idle], attackSpeed, isPrince);
+    //}
+
+
+    //protected void SetState(TowerState state, float speed)
+    //{
+    //    if (dicState.ContainsKey(state))
+    //    {
+    //        stateMachine.SetState(dicState[state], speed, isPrince);
+    //    }
+    //}
 }
